91bffe2081cf44f264118764d7dc7f313d013728
inamuu <8310973+inamuu@users.noreply.github.com>
2019-12-04 01:42:20 +0900

    :baby: 1st commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+import json
+import requests
+
+url = 'https://inamuu.com/wp-json/wp/v2/posts?per_page=1&search=ansible'
+headers = {"content-type": "application/json"}
+data = requests.get(url, headers=headers)
+jsondata = data.json()
+print(json.dumps(jsondata[0]["id"], indent=2))
+
