98ce553e1e15df9973665beeca4041e1ff15e6a0
inamuu <8310973+inamuu@users.noreply.github.com>
2019-12-04 02:00:12 +0900

    :shirt: ユニコード変換された文字列を日本語表示させる

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ url = 'https://inamuu.com/wp-json/wp/v2/posts?per_page=1&search=ansible'
 headers = {"content-type": "application/json"}
 data = requests.get(url, headers=headers)
 jsondata = data.json()
-print(json.dumps(jsondata[0]["id"], indent=2))
+print(json.dumps(jsondata[0]["title"]["rendered"], indent=2,ensure_ascii=False))
 
