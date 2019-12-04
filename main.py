f67265fc9436958971594f345ced874e83e0d8d0
inamuu <8310973+inamuu@users.noreply.github.com>
2019-12-04 21:31:05 +0900

    fix: 一旦リスト取得できるところまで

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 
 import json
 import requests
+import sys
 
-url = 'https://inamuu.com/wp-json/wp/v2/posts?per_page=10&search=ansible'
+url = "https://inamuu.com/wp-json/wp/v2/posts?per_page=10&search=ansible
 headers = {"content-type": "application/json"}
 data = requests.get(url, headers=headers)
 jsondata = data.json()
 
+search_list = []
 for i in jsondata:
-    print(json.dumps(i["title"]["rendered"], indent=2,ensure_ascii=False))
-    print(json.dumps(i["guid"]["rendered"], indent=2,ensure_ascii=False))
+    search_result = { "title" : i["title"]["rendered"], "arg" : i["guid"]["rendered"] }
+    search_list.append(search_result)
 
+
+dict_2 = json.dumps({ "items": search_list }, ensure_ascii=False)
+print(dict_2)
