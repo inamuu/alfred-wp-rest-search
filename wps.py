1946bad5b515eb11b333b9762acc37f6bb30fc8b
inamuu <8310973+inamuu@users.noreply.github.com>
2019-12-09 21:14:57 +0900

    :tada: サイトURLをファイルから読み出すように変更

--- wps.py
+++ wps.py
@@ -1,9 +1,17 @@
 # coding: utf-8
 
 import json
+import os
 import requests
 
-url = "https://inamuu.com/wp-json/wp/v2/posts?per_page=10&search={query}"
+path = str(os.environ['HOME']) + '/.alfred.workflow.wps.siteurl'
+
+if os.path.isfile(path) is True:
+    filepath = open(path, 'r')
+    for row in filepath:
+        siteurl = row.strip()
+
+url = siteurl + "/wp-json/wp/v2/posts?per_page=10&search={query}"
 headers = {"content-type": "application/json"}
 data = requests.get(url, headers=headers)
 jsondata = data.json()
@@ -13,6 +21,6 @@ for i in jsondata:
     search_result = { "title" : i["title"]["rendered"], "arg" : i["guid"]["rendered"] }
     search_list.append(search_result)
 
-
 postsdata = json.dumps({ "items": search_list }, ensure_ascii=False)
 print(postsdata)
+
